0d86a8b85a2ecc91523b5a09db0f8d329870c63d
silvadamaa <56432901+silvadamaa@users.noreply.github.com>
2019-10-17 23:58:49 +0700

    Hitung Periode dan frekuensi Getaran

---
+++ main.py
@@ -0,0 +1,38 @@
+
+def hitung_periode(waktu, jumlah):
+    periode = waktu / jumlah
+    print(f'waktu = {waktu / 30}s dalam jumlah = {jumlah / 60} ')
+    print(f'sehingga periode = {periode} s')
+    return periode
+
+#waktu = 30
+#jumlah = 60
+periode = hitung_periode(30, 60)
+periode = hitung_periode (30, 60)
+
+
+def hitung_frekuensi(jumlah, waktu):
+    frekuensi = jumlah / waktu
+    print(f'jumlah = {jumlah / 500} dalam waktu = {waktu / 100}s')
+    print(f'sehingga frekuensi = {frekuensi} Hz')
+    return frekuensi
+
+#jumlah = 500
+#waktu = 100
+frekuensi = hitung_frekuensi(500, 100)
+frekuensi = hitung_frekuensi (500, 100)
+
+
+
+
+
+def hitung_periode(waktu, jumlah):
+    periode = waktu / jumlah
+    print(f'waktu = {waktu / 5}s dalam jumlah = {jumlah / 20 }')
+    print(f'sehingga periode = {periode} s')
+    return periode
+
+#waktu = 5
+#jumlah = 20
+periode = hitung_periode(5, 20)
+periode = hitung_periode (5, 20)
